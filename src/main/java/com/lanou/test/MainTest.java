4a920ea17f1c582954366b506faf418a0d7b4038
sdlylzhr@163.com <sdlylzhr@163.com>
2017-09-28 15:19:02 +0800

    在MainTest文件中, 添加一行输出语句

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -5,6 +5,11 @@ package com.lanou.test;
  */
 public class MainTest {
     public static void main(String[] args) {
+        System.out.println("hahaha");
+
+
+
+
         System.out.println("别睡觉, 会死!");
     }
 }
