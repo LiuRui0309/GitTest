41251cd96a640eb90054381d1facb6e3004e53b0
sdlylzhr@163.com <sdlylzhr@163.com>
2017-09-28 15:12:54 +0800

    第一次提交, 添加工程文件

---
+++ src/main/java/com/lanou/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.lanou.test;
+
+/**
+ * Created by lizhongren1 on 2017/9/28.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("别睡觉, 会死!");
+    }
+}
